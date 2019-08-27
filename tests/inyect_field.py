0843b1330cff13acc86d2472f0d00daab04842b2
Patricio Labin <plabin@outlook.cl>
2019-08-26 20:53:43 -0400

    inyect field test placeholder

---
+++ tests/inyect_field.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+from ProfileMerger import Profile
+import models
+
+
+if __name__ == '__main__':
+    profile_test = Profile('test')
+    profile_test.scan_file('test_a.profile')
+
+    print('Pre injection')
+    pprint(profile_test.fields)
+
+    test_field = models.ProfileApplicationVisibility(
+        'TESTAPP', True, True
+    )
+    profile_test.fields[test_field.model_id] = test_field
+
+    print('Post Injection')
+    pprint(profile_test.fields)
+
+    profile_test.save_file(override_file_path='test_output.profile')
