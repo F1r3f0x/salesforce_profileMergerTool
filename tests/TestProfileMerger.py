b991abeeb5bed294809155066eb7ff3f181dce35
F1r3f0x <plabin@outlook.cl>
2024-05-18 21:58:02 -0400

    Add decent tests

---
+++ tests/TestProfileMerger.py
@@ -0,0 +1,22 @@
+import unittest
+from pprint import pprint
+
+from ProfileMerger import ProfileMerger, PROFILE_A, PROFILE_B, PROFILE_MERGED, Profile, ValueMerge
+import models
+
+
+class TestProfileMerger(unittest.TestCase):
+    def setUp(self):
+        self.profile_a = Profile(PROFILE_A, 'tests/test_a.profile')
+        self.profile_b = Profile(PROFILE_B, 'tests/test_b.profile')
+        self.merger = ProfileMerger(None, None, self.profile_a, self.profile_b)
+        
+        
+    def test_merge(self):
+        self.merger.merge_and_save()
+        pprint(self.merger.profile_merged.diffs)
+        self.assertTrue(self.merger.profile_merged.is_merged)
+        self.assertEqual(len(self.merger.profile_merged.diffs), 11)
+    
+if __name__ == '__main__':
+    unittest.main()
